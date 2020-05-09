f8a41ecc00e7a07743d41b39168718b6fcb40ed9
Pragati Rana <shalu.pragati11@gmail.com>
2020-05-09 19:17:32 +0530

    my project

---
+++ general.py
@@ -0,0 +1,9 @@
+import os
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print("Creating project"+directory)
+        os.makedirs(directory)
+
+create_project_dir("tracker")
+
