c687e1b882d53225072703fa1f0bcaf0cd5b6d0f
Pragati Rana <shalu.pragati11@gmail.com>
2020-05-09 21:16:40 +0530

    source

--- general.py
+++ general.py
@@ -5,5 +5,19 @@ def create_project_dir(directory):
         print("Creating project"+directory)
         os.makedirs(directory)
 
-create_project_dir("tracker")
+
+def create_data_files(project_name, base_url):
+    queue=project_name+'queue.txt'
+    crawled=project_name+'crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+def write_file(path, data):
+    f=open(path, 'w')
+    f.write(data)
+    f.close()
+
+
 
