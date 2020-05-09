6938ed4536ac0fae1f448816852e35026002a63f
Pragati Rana <shalu.pragati11@gmail.com>
2020-05-09 23:33:07 +0530

    upated

--- spider.py
+++ spider.py
@@ -31,8 +31,17 @@ class Spider:
         Spider.crawled=file_to_set(Spider.crawled_file)
 
     @staticmethod
-    def crawl_page():
-        
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name +'now crawling '+ page_url)
+            print('Queue' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
+
+
 
 
 
