9088901b8d8a798c66ab95887ea9c204c8f4ef7e
Pragati Rana <shalu.pragati11@gmail.com>
2020-05-09 21:35:40 +0530

    source

--- general.py
+++ general.py
@@ -7,8 +7,8 @@ def create_project_dir(directory):
 
 
 def create_data_files(project_name, base_url):
-    queue=project_name+'queue.txt'
-    crawled=project_name+'crawled.txt'
+    queue=project_name+'/queue.txt'
+    crawled=project_name+'/crawled.txt'
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(crawled):
@@ -20,4 +20,16 @@ def write_file(path, data):
     f.close()
 
 
+#add data onto an existing file
+
+def append_to_filename(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+#delete the contents of a file
+def delete_file_content(path):
+    with open(path, 'w'):
+        #do nothing 
+        pass
+    
 
