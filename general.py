f3aa81da20edfbf2b354d9bdc63d2596372cd091
Pragati Rana <shalu.pragati11@gmail.com>
2020-05-10 13:33:26 +0530

    updated

--- general.py
+++ general.py
@@ -1,23 +1,24 @@
 import os
 
+#Each website is a separate project (Folder)
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print("Creating project"+directory)
         os.makedirs(directory)
 
-
+#Create queue and crawled files (if not created)
 def create_data_files(project_name, base_url):
-    queue=project_name+'/queue.txt'
-    crawled=project_name+'/crawled.txt'
+    queue=os.path.join(project_name, 'queue.txt')
+    crawled=os.path.join(project_name, 'crawled.txt')
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(crawled):
         write_file(crawled, '')
 
+#Create a new file
 def write_file(path, data):
-    f=open(path, 'w')
-    f.write(data)
-    f.close()
+    with open(path, 'w') as f:
+        f.write(data)
 
 
 #add data onto an existing file
@@ -28,9 +29,7 @@ def append_to_filename(path, data):
 
 #delete the contents of a file
 def delete_file_content(path):
-    with open(path, 'w'):
-        #do nothing 
-        pass
+    open(path, 'w').close()
 
 #read a file and convert each line to set items
 def file_to_set(file_name):
@@ -40,10 +39,10 @@ def file_to_set(file_name):
             results.add(line.replace('\n', ''))
     return results
 
-#iterate through a set, each item will be a new ine in a file
-def set_to_file(links, file):
-    delete_file_content(file)
-    for link in sorted(links):
-        append_to_filename(file, link)
+#iterate through a set, each item will be a new line in a file
+def set_to_file(links, file_name):
+    with open(file_name, 'w') as f:
+        for l in sorted(links):
+            f.write(l+"\n")
     
 
